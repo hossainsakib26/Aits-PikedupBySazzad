7e1d00aab211cd63c8844874d6930ed6796ea1a8
Sazzad Hossain <hossain.sakib26@hotmail.com>
2021-07-10 12:33:19 +0600

    first commit

---
+++ Aits-PikedupBySazzad/Aits-PikedupBySazzad/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aits_PikedupBySazzad.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
